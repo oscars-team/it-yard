f17d9f4c893b0263985a467d684ede552943e647
hakunamatata <xymbtc@163.com>
2019-01-18 14:48:53 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
     { path: 'content-view', loadChildren: './pages/content-view/content-view.module#ContentViewPageModule' },
-    { path: 'channel', loadChildren: './pages/channel/channel.module#ChannelPageModule' },  { path: 'web-browser', loadChildren: './pages/web-browser/web-browser.module#WebBrowserPageModule' }
+    { path: 'channel', loadChildren: './pages/channel/channel.module#ChannelPageModule' }
 
 
 ];
